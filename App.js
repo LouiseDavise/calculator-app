f6e5710ddd902d9753b6a4b9f54cf59b86bbbc7f
LouiseDavise <louizkwok@gmail.com>
2025-01-10 23:37:16 +0800

    Screen Design

--- App.js
+++ App.js
@@ -1,31 +1,198 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableHighlight } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faDivide, faXmark, faMinus, faEquals, faPercent, faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
+import { create, all } from 'mathjs'
+
+const config = {}
+const math = create(all, config)
 
 const App = () => {
+	const [count, setCount] = useState(0);
+	const [titleText, setTitleText] = useState("");
+	const [valueText, setValueText] = useState("");
+	const [result, setResult] = useState("");
+
+	const onPressButton9 = () => {
+		setTitleText(titleText.concat("9"));
+	};
+
+	const onPressButton8 = () => {
+		setTitleText(titleText.concat("8"));
+	};
+
+	const onPressButton7 = () => {
+		setTitleText(titleText.concat("7"));
+	};
+
+	const onPressButton6 = () => {
+		setTitleText(titleText.concat("6"));
+	};
+
+	const onPressButton5 = () => {
+		setTitleText(titleText.concat("5"));
+	};
+
+	const onPressButton4 = () => {
+		setTitleText(titleText.concat("4"));
+	};
+
+	const onPressButton3 = () => {
+		setTitleText(titleText.concat("3"));
+	};
+
+	const onPressButton2 = () => {
+		setTitleText(titleText.concat("2"));
+	};
+
+	const onPressButton1 = () => {
+		setTitleText(titleText.concat("1"));
+	};
+
+	const onPressButton0 = () => {
+		setTitleText(titleText.concat("0"));
+	};
+
+	const onPressButtonAddition = () => {
+		let lastValue = titleText.substring(titleText.length - 1);
+		if (lastValue == "+" || lastValue == "−" || lastValue == "×" || lastValue == "÷") {
+			setTitleText(titleText.slice(0, -1).concat("+"));
+		} else {
+			setTitleText(titleText.concat("+"));
+		}
+	};
+
+	const onPressButtonMultiple = () => {
+		let lastValue = titleText.substring(titleText.length - 1);
+		if (lastValue == "+" || lastValue == "−" || lastValue == "×" || lastValue == "÷") {
+			setTitleText(titleText.slice(0, -1).concat("×"));
+		} else {
+			setTitleText(titleText.concat("×"));
+		}
+	};
+
+	const onPressButtonSubstitute = () => {
+		let lastValue = titleText.substring(titleText.length - 1);
+		if (lastValue == "+" || lastValue == "−" || lastValue == "×" || lastValue == "÷") {
+			setTitleText(titleText.slice(0, -1).concat("−"));
+		} else {
+			setTitleText(titleText.concat("−"));
+		}
+	};
+
+	const onPressButtonDivide = () => {
+		let lastValue = titleText.substring(titleText.length - 1);
+		if (lastValue == "+" || lastValue == "−" || lastValue == "×" || lastValue == "÷") {
+			setTitleText(titleText.slice(0, -1).concat("÷"));
+		} else {
+			setTitleText(titleText.concat("÷"));
+		}
+	};
+
+	const onPressButtonPercentage = () => {
+		setTitleText(titleText.concat("%"));
+	}
+
+	const getValueBeforeLastOperator = (input) => {
+		const lastOperatorIndex = Math.max(
+			input.lastIndexOf("+"),
+			input.lastIndexOf("−"),
+			input.lastIndexOf("×"),
+			input.lastIndexOf("÷")
+		);
+		return input.substring(lastOperatorIndex + 1);
+	};
+
+	const onPressButtonPosMin = () => {
+		let value = getValueBeforeLastOperator(titleText);
+		initialValueLength = value.length;
+		if (value.length > 0) {
+			if (value[0] === '(' && value[1] === '-' && value[value.length - 1] === ')') {
+				value = value.slice(2, -1);
+				setTitleText(titleText.slice(0, -initialValueLength).concat(value));
+			} else {
+				value = `(-${value})`;
+				setTitleText(titleText.slice(0, -initialValueLength).concat(value));
+			}
+		}
+	}
+
+	const onPressButtonDot = () => {
+		const value = getValueBeforeLastOperator(titleText);
+		if (value.indexOf(".") === -1) {
+			setTitleText(titleText.concat("."));
+		}
+	}
+
+	const onPressButtonAC = () => {
+		setTitleText("");
+		setResult("");
+	};
+
+	const onPressButtonBackSpace = () => {
+		let afterBackSpace = titleText.substring(0, titleText.length - 1);
+		setTitleText(afterBackSpace);
+	};
+
+	const onPressButtonResult = () => {
+		try {
+			let equation = titleText
+				.replace(/×/g, "*")
+				.replace(/÷/g, "/")
+				.replace(/−/g, "-");
+
+			let result = math.evaluate(equation);
+
+			result = math.format(result, { precision: 14 });
+
+			setResult(result.toString());
+		} catch (error) {
+			console.error("Invalid equation:", error);
+			setResult("Error");
+		}
+	};
+
 	return (
 		<SafeAreaProvider>
 			<SafeAreaView style={styles.container}>
 				<View style={styles.calculateSpace}>
-
+					{/* <Text style={styles.inputText}>
+						{titleText}
+					</Text> */}
+					<TextInput
+						style={styles.inputText}
+						value={titleText}
+						onChangeText={(text) => setTitleText(text)}
+						keyboardType="none"
+						placeholder="0" // Optional: Placeholder text
+						placeholderTextColor="#555555"
+						selection={{ start: titleText.length, end: titleText.length }} // Handle cursor positioning
+						autoCorrect={false} // Disable auto-correction
+						autoCapitalize="none" // Disable auto-capitalization
+						onSelectionChange={(event) => {
+							console.log("Cursor Position:", event.nativeEvent.selection);
+						}}
+					/>
+					<Text style={styles.inputResult}>
+						{result}
+					</Text>
 				</View>
 				<View style={styles.inputSpace}>
 					<View style={styles.rowContainer}>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed')}
+							onPress={onPressButtonAC}
 							style={styles.button}>
 							<View style={[styles.button]}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>AC</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonBackSpace}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>
@@ -34,18 +201,18 @@ const App = () => {
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonPosMin}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>+/-</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonDivide}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>
@@ -56,36 +223,36 @@ const App = () => {
 					</View>
 					<View style={styles.rowContainer}>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton7}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>7</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton8}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>8</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton9}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>9</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonMultiple}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>
@@ -96,36 +263,36 @@ const App = () => {
 					</View>
 					<View style={styles.rowContainer}>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton4}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>4</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton5}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>5</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton6}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>6</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonSubstitute}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>
@@ -136,36 +303,36 @@ const App = () => {
 					</View>
 					<View style={styles.rowContainer}>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton1}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>1</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton2}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>2</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton3}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>3</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonAddition}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>+</Text>
@@ -174,9 +341,9 @@ const App = () => {
 					</View>
 					<View style={styles.rowContainer}>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonPercentage}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={[styles.buttonTextColor, styles.specialButtonTextColor]}>
@@ -185,27 +352,27 @@ const App = () => {
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButton0}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>0</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonDot}
 							style={styles.button}>
 							<View style={styles.button}>
 								<Text style={styles.buttonTextColor}>.</Text>
 							</View>
 						</TouchableHighlight>
 						<TouchableHighlight
-							activeOpacity={0.6}
+							activeOpacity={0.8}
 							underlayColor="#DDDDDD"
-							onPress={() => alert('Pressed!')}
+							onPress={onPressButtonResult}
 							style={[styles.button]}>
 							<View style={[styles.button, styles.specialButtonBackground]}>
 								<Text style={[styles.buttonTextColor]}>
@@ -227,6 +394,8 @@ const styles = StyleSheet.create({
 	calculateSpace: {
 		flex: 0.5,
 		backgroundColor: '#000000',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
 	},
 	inputSpace: {
 		flex: 0.5,
@@ -243,8 +412,8 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: '#0F0F0F',
-		width: 70,
-		height: 60,
+		width: 80,
+		height: 70,
 		borderRadius: 100,
 	},
 	buttonTextColor: {
@@ -256,6 +425,17 @@ const styles = StyleSheet.create({
 	},
 	specialButtonBackground: {
 		backgroundColor: '#0073E6',
+	},
+	inputText: {
+		fontSize: 55,
+		fontWeight: 'semibold',
+		color: 'white',
+		marginHorizontal: 15,
+	},
+	inputResult: {
+		fontSize: 20,
+		color: '#BDBDBD',
+		marginHorizontal: 15,
 	},
 })
 
